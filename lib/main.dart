dc9a373ac5990011dcce1440173f9edf276f5da6
mac <>
2021-02-02 16:37:20 +0700

    非命名路由传参

--- lib/main.dart
+++ lib/main.dart
@@ -19,14 +19,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     // MaterialUI库中提供的UI架构
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter 示例',
       theme: ThemeData(
         // 应用的主题
         primarySwatch: Colors.blue, // 可选颜色 Colors.green等
         visualDensity: VisualDensity
             .adaptivePlatformDensity, // 密度（可选-4到4之间的值，越小表示越密集/更紧凑。此处的值表示。此处的值表示：台式机平台使用紧凑型，其他平台按规范来）
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'), // 首页部件
+      home: MyHomePage(title: 'flutter 首页'), // 首页部件
     );
   }
 }
@@ -59,6 +59,7 @@ class _MyHomePageState extends State<MyHomePage> {
       _counter++;
     });
   }
+
   @override
   // 部件构建/重新构建（每次调用 setState 方法都会重新运行此方法）
   Widget build(BuildContext context) {
@@ -77,12 +78,60 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center, // 垂直居中
           children: <Widget>[
             Text(
-              'You have pushed the buttonbuttonbuttonbuttonbuttonbuttonbuttonbuttonbuttonbutton this many times:',
+              'clumn 的文字部件，我是文字部件，我还是第一个文字部件哟第一一一一一一一一一一一一一一一一一一一一',
             ),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headline4,
             ),
+            // 跳转新路由的按钮
+            FlatButton(
+              child: Text("打开新路由"),
+              textColor: Colors.blue, // 文字为蓝色
+              onPressed: () {
+                /*
+                 * 导航到新路由
+                 * Navigator 是一个路由管理的组件，它提供了打开和退出路由页方法。
+                 * Navigator 通过一个栈来管理活动路由集合。通常当前屏幕显示的页面就是栈顶的路由。
+                 * 常用方法1: Navigator.push(BuildContext context, Route route) 将给定的路由入栈（即打开新的页面），返回值是一个 future 对象，用以接收新路由出栈（即关闭）时的返回数据。
+                 * 常用方法2: Navigator.pop(BuildContext context, [result]) 将栈顶路由出栈， result 为页面关闭时返回给上一个页面的数据。
+                 * Navigator 还有很多其它方法，如 Navigator.replace 、 Navigator.popUntil 等。
+                 */
+                Navigator.push(
+                  context,
+                  /**
+                   * MaterialPageRoute 方法继承自抽象类 PageRoute ，是 Material 组件库提供的组件，它可以针对不同平台，实现与平台页面切换动画风格一致的路由切换动画。
+                   * 使用方法: MaterialPageRoute(WidgetBuilder builder, RouteSettings settings, bool maintainState = true, bool fullscreenDialog = false,) 
+                   * 参数 builder          是一个 WidgetBuilder 类型的回调函数，它的作用是构建路由页面的具体内容，返回值是一个 widget 。我们通常要实现此回调，返回新路由的实例。
+                   * 参数 settings         包含路由的配置信息，如路由名称、是否初始路由（首页）。
+                   * 参数 maintainState    默认情况下，当入栈一个新路由时，原来的路由仍然会被保存在内存中，如果想在路由没用的时候释放其所占用的所有资源，可以设置 maintainState 为 false 。
+                   * 参数 fullscreenDialog 表示新的路由页面是否是一个全屏的模态对话框，在 iOS 中，如果 fullscreenDialog 为 true ，新页面将会从屏幕底部滑入（而不是水平方向）。
+                   */
+                  MaterialPageRoute(builder: (context) {
+                    return NewRoute();
+                  })
+                );
+              },
+            ),
+            // 打开参数按钮
+            RaisedButton(
+              child: Text("打开接收参数的非命名路由"),
+              textColor: Colors.orange[900], // 文字为蓝色
+              onPressed: () async {
+                // 通过等待 Navigator.push() 返回的 future 来获取非命名路由的返回数据。
+                var result = await Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return ParamRoute(
+                        text: "我是非命名路由传过来的参数文本", // 路由参数文本
+                      );
+                    },
+                  ),
+                ); // 打开`ParamRoute`，并等待返回结果
+                print("非命名路由返回值是: $result"); // 输出 ParamRoute 非命名路由出栈时返回的结果
+              },
+            )
           ],
         ),
       ),
@@ -92,6 +141,57 @@ class _MyHomePageState extends State<MyHomePage> {
         tooltip: 'Increment',
         child: Icon(Icons.add),
       ),
+    );
+  }
+}
+
+// 创建新路由
+class NewRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('新路由'),
+      ),
+      body: Center(
+        child: Text('这是新路由')
+      )
+    );
+  }
+}
+
+/*
+ * 非命名路由传值参数
+ * 在 ParamRoute 页中有两种方式可以返回到上一页；第一种方式是直接点击导航栏返回箭头（不会返回数据给上一个路由），第二种方式是点击页面中的“返回”按钮（会返回数据给上一个路由）。
+ */
+class ParamRoute extends StatelessWidget {
+  ParamRoute({
+    Key key,
+    @required this.text,  // 接收一个传递过来的 text 参数
+  }) : super(key: key);
+  final String text; // 定义一个 final 类型的字符串（把传递过来的 text 参数赋值给这个字符串）
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('参数路由'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(18),
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              Text(text),
+              RaisedButton(
+                child: Text('返回'),
+                textColor: Colors.orange[900], // 文字为蓝色
+                onPressed: () => Navigator.pop(context, '1参数2路由3返回值4'),
+              ),
+              Text('非命名路由出栈时传递的参数：1参数2路由3返回值4')
+            ],
+          ),
+        )
+      )
     );
   }
 }
