2bb582d2483b70051777dfa39b620abd5a9c22dd
zhan1.ba0999@gmail.com <zhan1.ba0999@gmail.com>
2021-04-30 17:12:41 +0700

    添加-provider说明

--- lib/store/testStatus.dart
+++ lib/store/testStatus.dart
@@ -5,8 +5,13 @@ import 'package:flutter/foundation.dart';
 /// 主要用来处理一些多部件的状态保存，和 shared_preferences 有区别。
 /// shared_preferences 是本地保存，这种方式是多部件共享的状态。
 class TestState with ChangeNotifier {
+  /// 定义
   bool test = false;
+
+  /// 获取
   bool get isTest => test;
+
+  /// 设置
   void setTest() {
     test = !test;
     notifyListeners();
